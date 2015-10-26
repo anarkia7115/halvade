da9ea6f8669eba18b8f56dc3a174ca92f3ed88dc
Mark Gao <marc7115@gmail.com>
2015-10-26 13:09:42 +0800

    new

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-aws s3 cp ./target/halvade-1.0-SNAPSHOT-jar-with-dependencies.jar s3://gcbibucket/halvade/
+aws s3 cp ./target/halvade-1.0-SNAPSHOT-jar-with-dependencies.jar s3://gcbibucket/halvade/HalvadeWithLibs.jar
